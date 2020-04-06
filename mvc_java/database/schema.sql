75cb4100a87eb9d7faa238145580d1e459dacd3f
Tanner Cole <tannerleecole@gmail.com>
2020-04-06 12:43:18 -0400

    built a possible structure for how keyword response matching can work

--- mvc_java/database/schema.sql
+++ mvc_java/database/schema.sql
@@ -6,6 +6,25 @@ BEGIN;
 
 -- CREATE statements go here
 DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS keywords;
+DROP TABLE IF EXISTS responses;
+
+CREATE TABLE responses (
+
+        id serial PRIMARY KEY,
+        answer varchar NOT NULL,
+        title varchar NOT NULL
+
+);
+
+CREATE TABLE keywords (
+
+        keywordid int NOT NULL,
+        words varchar NOT NULL,
+        
+        CONSTRAINT fk_id FOREIGN KEY (keywordid) REFERENCES responses (id)
+
+);
 
 CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
