c15715d067d66bbffa8c7379bcc4396571c8db39
Devin Beichner <devbeichner@gmail.com>
2020-04-06 18:24:06 -0400

    Authentication issues resolved, should be able to log in and register now

--- vue_starter_java/backend/src/main/java/com/techelevator/controller/AccountController.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/controller/AccountController.java
@@ -2,22 +2,27 @@ package com.techelevator.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.techelevator.authentication.AuthProvider;
 import com.techelevator.authentication.JwtTokenHandler;
 import com.techelevator.authentication.UnauthorizedException;
 import com.techelevator.authentication.UserCreationException;
 import com.techelevator.model.User;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 /**
  * AccountController
  */
 @RestController
+@CrossOrigin
 public class AccountController {
     @Autowired
     private AuthProvider auth;
