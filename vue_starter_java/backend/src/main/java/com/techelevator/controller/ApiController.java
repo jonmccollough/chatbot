d190328f22d194d42ed35a6f319985daebab72c4
Tanner Cole <tannerleecole@gmail.com>
2020-04-07 08:05:06 -0400

    starting building in basic methodology for our api to return answers from a database based on keywords

--- vue_starter_java/backend/src/main/java/com/techelevator/controller/ApiController.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/controller/ApiController.java
@@ -2,8 +2,13 @@ package com.techelevator.controller;
 
 import com.techelevator.authentication.AuthProvider;
 import com.techelevator.authentication.UnauthorizedException;
+import com.techelevator.model.Message;
+import com.techelevator.model.MessageDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
  * ApiController
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ApiController {
 
     @Autowired
     private AuthProvider authProvider;
 
+    @Autowired
+    private MessageDao messageDao;
+    
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String authorizedOnly() throws UnauthorizedException {
         /*
@@ -32,4 +41,10 @@ public class ApiController {
         }
         return "Success";
     }
+    
+    @GetMapping("/{keyword}")
+	public Message searchForResponse(@PathVariable String keyword) {
+		return messageDao.searchByKeyword(keyword);
+	}
+    
 }
