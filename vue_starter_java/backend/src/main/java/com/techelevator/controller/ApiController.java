71e294b5b4538f15e281cee3187449b552687877
Tanner Cole <tannerleecole@gmail.com>
2020-04-08 12:21:29 -0400

    updating controller

--- vue_starter_java/backend/src/main/java/com/techelevator/controller/ApiController.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/controller/ApiController.java
@@ -1,9 +1,9 @@
 package com.techelevator.controller;
 
-import com.techelevator.authentication.AuthProvider;
-import com.techelevator.authentication.JwtAuthInterceptor;
-import com.techelevator.authentication.JwtTokenHandler;
-import com.techelevator.authentication.UnauthorizedException;
+//import com.techelevator.authentication.AuthProvider;
+//import com.techelevator.authentication.JwtAuthInterceptor;
+//import com.techelevator.authentication.JwtTokenHandler;
+//import com.techelevator.authentication.UnauthorizedException;
 import com.techelevator.model.Message;
 import com.techelevator.model.MessageDao;
 
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,8 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
-    @Autowired
-    private AuthProvider authProvider;
+//    @Autowired
+//    private AuthProvider authProvider;
 
     @Autowired
     private MessageDao messageDao;
