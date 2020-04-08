ae3632b626ad800bb3091f9edaae8cb5225bbd56
Jarret Fawcett <jarret.fawcett@gmail.com>
2020-04-08 12:24:25 -0400

    f

--- vue_starter_java/frontend/src/App.vue
+++ vue_starter_java/frontend/src/App.vue
@@ -3,17 +3,15 @@
     
     <chatbot/>
   </div>
+
 </template>
 
 <script>
 import chatbot from '@/components/chatbot.vue';
-
-
 export default {
 name: 'app',
 components: {
   chatbot,
- 
 }
 }
 </script>
