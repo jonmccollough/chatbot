895c97499420e1e122ed3bc11d8614afb5d40f4e
Jonathon McCollough <jonathonmccollough@gmail.com>
2020-04-09 17:11:21 -0400

    fixing up faq. cant figure out why nav bar is hiding

--- vue_starter_java/frontend/src/components/faq.vue
+++ vue_starter_java/frontend/src/components/faq.vue
@@ -2,8 +2,6 @@
 
 <body>
     <div class="faq" ref = "faq"></div>
-<template>
-    <body>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="https://www.techelevator.com" target="_blank">
@@ -72,36 +70,71 @@
            </div>
         </section> 
     </section>
-    <footer class="footer">
-        <div class="container">
-            <div class="content has-text-centered">
-                <p class="has-text-success">remove me</p>
-            </div>
+<section>
+<div>
+    <h1>  FAQ 
+        WORDS N STUFF HERE
+
+
+    </h1>
+</div>
+</section>
+<footer class="footer">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-one-third">
+          <div class="is-pulled-left">
+            <p>
+              <a class="has-text-grey" href="http://www.techelevator.com">tech elevator</a>
+            </p>
+            <p>
+              <a class="has-text-grey" href="mailto:hello@techelevator.com">hello@techelevator.com</a>
+            </p>
+            <p>
+              <a class="has-text-grey" href="tel:8776063203">877.606.3203</a>
+            </p>
+          </div>
         </div>
-    </footer>
-    </body>
-</template>
-<br>
-<h1>  FAQ 
-    WORDS N STUFF HERE
-
-
-</h1>
-
-
-    <footer class="footer">
-        <div class="container">
-            <div class="content has-text-centered">
-                <p class="has-text-success">remove me</p>
-            </div>
+        <div class="column">
+          <div class="is-centered">
+            <p>
+              <a class="has-text-grey" href="http://www.techeelevator.com/events">events</a>
+            </p>
+            <p>
+              <a class="has-text-grey">faqs</a>
+            </p>
+          </div>
         </div>
-    </footer>
-    
+
+        <div class="column">
+          <div class="is-pulled-right">
+            <figure class="image is-24x24">
+              <a href="https://facebook.com/techelevator">
+                <img src="@/img/fb.png" />
+              </a>
+              <a href="https://instagram.com/techelevator">
+                <img src="@/img/ig.png" />
+              </a>
+              <a href="https://linkedin.com/school/tech_elevator">
+                <img src="@/img/li.png" />
+              </a>
+              <a href="https://twitter.com/Tech_Elevator">
+                <img src="@/img/tw.png" />
+              </a>
+              <a href="https://youtube.com/techelevatorcodeschool">
+                <img src="@/img/yt.png" />
+              </a>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
 </body>
-   
 </template>
-<script>
 
+<script>
 export default {
   name: 'faq',
   components: {
