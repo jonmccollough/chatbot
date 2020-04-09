80cf178f63f7012ee4165c84fc782fde2631ab94
Jonathon McCollough <jonathonmccollough@gmail.com>
2020-04-09 16:34:38 -0400

    fixed FAQ nav and added new tab opens for official TE site stuff

--- vue_starter_java/frontend/src/components/chatbot.vue
+++ vue_starter_java/frontend/src/components/chatbot.vue
@@ -2,7 +2,7 @@
 <body>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://www.techelevator.com">
+      <a class="navbar-item" href="https://www.techelevator.com" target="_blank">
         <img
           src="//static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/5dceda76b702913cebc1d05e/1586375316161/?format=1500w"
           alt="Tech Elevator Coding Bootcamp "
@@ -25,14 +25,14 @@
 
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item is-family-sans-serif">CHATBOT</a>
+        <a class="navbar-item is-family-sans-serif" href="/">CHATBOT</a>
 
-        <a class="navbar-item is-family-sans-serif">FAQS</a>
+        <a class="navbar-item is-family-sans-serif" href="faq">FAQS</a>
 
         <div class="navbar-item has-dropdown is-hoverable">
           <a
             class="navbar-item is-family-sans-serif"
-            href="http://www.techelevator.com/events"
+            href="http://www.techelevator.com/events" target="_blank"
           >CALENDAR</a>
         </div>
       </div>
--- vue_starter_java/frontend/src/views/Register.vue
+++ vue_starter_java/frontend/src/views/Register.vue
@@ -4,7 +4,7 @@
 
 <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://www.techelevator.com">
+      <a class="navbar-item" href="https://www.techelevator.com" target="_blank">
         <img
           src="//static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/5dceda76b702913cebc1d05e/1586375316161/?format=1500w"
           alt="Tech Elevator Coding Bootcamp "
@@ -34,7 +34,7 @@
         <div class="navbar-item has-dropdown is-hoverable">
           <a
             class="navbar-item is-family-sans-serif"
-            href="http://www.techelevator.com/events"
+            href="http://www.techelevator.com/events" target="_blank"
           >CALENDAR</a>
         </div>
       </div>
