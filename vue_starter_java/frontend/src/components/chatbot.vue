e5d0d01b53ac597fc81235c1ff4306558252e2b2
Jarret Fawcett <jarret.fawcett@gmail.com>
2020-04-10 10:59:32 -0400

    Chopped up header and footer

--- vue_starter_java/frontend/src/components/chatbot.vue
+++ vue_starter_java/frontend/src/components/chatbot.vue
@@ -1,59 +1,5 @@
 <template>
 <body>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://www.techelevator.com" target="_blank">
-        <img
-          src="//static1.squarespace.com/static/55ef2da9e4b03f6e1ef0cd28/t/5dceda76b702913cebc1d05e/1586375316161/?format=1500w"
-          alt="Tech Elevator Coding Bootcamp "
-          class="Header-branding-logo"
-        />
-      </a>
-
-      <a
-        role="button"
-        class="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item is-family-sans-serif" href="/">CHATBOT</a>
-
-        <a class="navbar-item is-family-sans-serif" href="faq">FAQS</a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a
-            class="navbar-item is-family-sans-serif"
-            href="http://www.techelevator.com/events" target="_blank"
-          >CALENDAR</a>
-        </div>
-      </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <router-link :to="{ name: 'register' }">
-              <a class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-            </router-link>
-            <router-link :to="{ name: 'login' }">
-              <a class="button is-light">Log in</a>
-            </router-link>
-          </div>
-        </div>
-      </div>
-    </div>
-  
-</nav>
     <section class ="container">
        <section class="hero is-small is-family-sans-serif is-info">
            <div class="hero-body">
@@ -93,57 +39,7 @@
                 <button @click="sendMessage" class="button is-dark">Send</button>
             </div>
     </section>
-  <footer class="footer">
-    <div class="container">
-      <div class="columns">
-        <div class="column is-one-third">
-          <div class="is-pulled-left">
-            <p>
-              <a class="has-text-grey" href="http://www.techelevator.com">tech elevator</a>
-            </p>
-            <p>
-              <a class="has-text-grey" href="mailto:hello@techelevator.com">hello@techelevator.com</a>
-            </p>
-            <p>
-              <a class="has-text-grey" href="tel:8776063203">877.606.3203</a>
-            </p>
-          </div>
-        </div>
-        <div class="column">
-          <div class="is-centered">
-            <p>
-              <a class="has-text-grey" href="http://www.techeelevator.com/events">events</a>
-            </p>
-            <p>
-              <a class="has-text-grey">faqs</a>
-            </p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="is-pulled-right">
-            <figure class="image is-24x24">
-              <a href="https://facebook.com/techelevator">
-                <img src="@/img/fb.png" />
-              </a>
-              <a href="https://instagram.com/techelevator">
-                <img src="@/img/ig.png" />
-              </a>
-              <a href="https://linkedin.com/school/tech_elevator">
-                <img src="@/img/li.png" />
-              </a>
-              <a href="https://twitter.com/Tech_Elevator">
-                <img src="@/img/tw.png" />
-              </a>
-              <a href="https://youtube.com/techelevatorcodeschool">
-                <img src="@/img/yt.png" />
-              </a>
-            </figure>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  
   </body>
 </template>
 
@@ -431,6 +327,8 @@ body {
   background-color: white;
   padding-left: 10px;
   padding-right: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
 }
 .chat-bot-list-container{
   display: flex;
