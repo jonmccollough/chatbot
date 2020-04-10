60fdc8e578b3eefdef16961431839d03c0a7b8b3
Jonathon McCollough <jonathonmccollough@gmail.com>
2020-04-10 14:36:47 -0400

    bot talk

--- vue_starter_java/backend/database/data.sql
+++ vue_starter_java/backend/database/data.sql
@@ -172,3 +172,7 @@ INSERT INTO responses (answer, title) VALUES ('Tech Elevator maintains a list of
 INSERT INTO keywords (words, keywordid) VALUES ('mental', (SELECT id FROM responses WHERE title = 'mentalhealth'));
 INSERT INTO keywords (words, keywordid) VALUES ('killmyself', (SELECT id FROM responses WHERE title = 'mentalhealth'));
 INSERT INTO keywords (words, keywordid) VALUES ('freakingout', (SELECT id FROM responses WHERE title = 'mentalhealth'));
+
+INSERT INTO responses (answer, title) VALUES ('Here is a map of popular tourist locations in Pittsburgh: <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d48577.604487171695!2d-80.00240115591183!3d40.4509174073966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1stourist%20sites%20near%20Pittsburgh%2C%20PA!5e0!3m2!1sen!2sus!4v1586539721618!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>', 'pittsburghtour');
+INSERT INTO keywords (words, keywordid) VALUES ('pittsburghtour', (SELECT id FROM responses WHERE title = 'pittsburghtour'));
+
